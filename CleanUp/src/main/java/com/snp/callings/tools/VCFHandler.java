f826b776359fa8b6ff8ce132dbdcdd49c44bf180
Jimmy JIN <Shengmin_Jin@URMC.Rochester.edu>
2015-07-23 15:55:29 -0400

    change

--- CleanUp/src/main/java/com/snp/callings/tools/VCFHandler.java
+++ CleanUp/src/main/java/com/snp/callings/tools/VCFHandler.java
@@ -13,42 +13,19 @@ public class VCFHandler {
      * 读取CSV文件
      */
      public void  readeCsv(){
-         /*try {    
-              
-             ArrayList<String[]> csvList = new ArrayList<String[]>(); //用来保存数据
-             String csvFilePath = "c:/test.csv";
-              CsvReader reader = new CsvReader(csvFilePath,',',Charset.forName("SJIS"));    //一般用这编码读就可以了    
-              
-              reader.readHeaders(); // 跳过表头   如果需要表头的话，不要写这句。
-              
-              while(reader.readRecord()){ //逐行读入除表头的数据    
-                  csvList.add(reader.getValues());
-              }            
-              reader.close();
-              
-              for(int row=0;row<csvList.size();row++){
-                  
-                  String  cell = csvList.get(row)[0]; //取得第row行第0列的数据
-                  System.out.println(cell);
-                  
-              }
-              
-              
-         }catch(Exception ex){
-             System.out.println(ex);
-         }*/
 		try {
 			
 			CsvReader products = new CsvReader("c:/test.csv", '\t');
 			//products.readHeaders();
 			while (products.readRecord())
 			{
-				String productID = products.get("A");
-				String productName = products.get("B");
-				
-				// perform program logic here
-				//System.out.println(products.getCurrentRecord());
-				System.out.println(products.getRawRecord());
+				String rec = products.getRawRecord();
+				if (rec.startsWith("##")) continue;
+				if (rec.startsWith("#"))
+				{
+					System.out.println(rec.substring(1));
+					
+				}
 			}
 			//System.out.println(products.get);
 		
